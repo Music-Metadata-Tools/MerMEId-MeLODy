a091f1f2bb597f9649a82c00ae9fe36fe2a1c1b1
claudius <claudius.teodorescu@gmail.com>
2024-09-29 22:11:56 +0300

    Now can edit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,69 @@
 import http from "https://unpkg.com/isomorphic-git@beta/http/web/index.js"
 
+// ontologies
+const classifications = {
+    "https://mei-metadata.org/classification/1": "text",
+    "https://mei-metadata.org/classification/2": "manuscript",
+    "https://mei-metadata.org/classification/3": "autograph",
+    "https://mei-metadata.org/classification/4": "sketch",
+    "https://mei-metadata.org/classification/5": "fragment"
+};
+
+const repositories = {
+    "https://mei-metadata.org/repository/1": "D-B",
+    "https://mei-metadata.org/repository/2": "D-LEm",
+    "https://mei-metadata.org/repository/3": "D-Hs",
+};
+
+const person_names = {
+    "https://mei-metadata.org/persons/1": "Erdmann Neumeister",
+    "https://mei-metadata.org/persons/2": "Georg Michael Telemann",
+    "https://mei-metadata.org/persons/3": "Georg Philipp Telemann",
+};
+
+const roles = {
+    "https://mei-metadata.org/Arranger": "arranger",
+    "https://mei-metadata.org/Author": "author",
+    "https://mei-metadata.org/Composer": "composer",
+    "https://mei-metadata.org/Contributor": "contributor",
+    "https://mei-metadata.org/Editor": "editor",
+    "https://mei-metadata.org/Funder": "funder",
+    "https://mei-metadata.org/Librettist": "librettist",
+    "https://mei-metadata.org/Lyricist": "lyricist",
+    "https://mei-metadata.org/Sponsor": "sponsor",
+};
+
+// templates
+const source_template = (data) =>
+    `
+         <manifestation>
+            <identifier label=""/>
+            <titleStmt>
+               <title>${data.title}</title>
+               ${generate_person_records(data.persons)}
+            </titleStmt>
+            <classification>
+               <termList>
+                  <term>${data.classification}</term>
+               </termList>
+            </classification>
+            <itemList>
+               <item>
+                  <physLoc>
+                     <repository>
+                        <identifier auth.uri="http://www.rism.info" auth="RISM">${data.repository}</identifier>
+                     </repository>
+                  </physLoc>
+               </item>
+            </itemList>
+         </manifestation>
+`;
+
+const person_template = (data) => `<${data.role}>${data.name}</${data.role}>`;
+
+// util functions
+const generate_person_records = (persons) => Object.values(persons).map(item => person_template(item)).join("");
+
 // Initialize isomorphic-git with a file system
 window.fs = new LightningFS("mermeid");
 window.pfs = window.fs.promises;
@@ -33,3 +97,62 @@ for (const file of files) {
     treeItem.innerText = file;
     lazyItem.append(treeItem);
 }
+
+const form = document.getElementById("shacl-form");
+const output = document.getElementById("shacl-output");
+
+/*form.addEventListener("change", (event) => {
+    output.classList.toggle('valid', event.detail?.valid);
+    output.classList.toggle('invalid', !event.detail?.valid);
+    output.querySelector("pre").innerText = form.serialize();
+
+    console.log(form.serialize());
+});*/
+
+document.addEventListener("click", (event) => {
+    let target = event.target;
+
+    if (target.matches("button#save")) {
+        let result = JSON.parse(form.serialize("application/ld+json"));
+
+        let title = "";
+        let classification = "";
+        let repository = "";
+        let persons = {};
+        result.forEach((item) => {
+            if (item.hasOwnProperty("https://mei-metadata.org/Title")) {
+                title = item["https://mei-metadata.org/Title"]["@value"];
+            }
+            if (item.hasOwnProperty("https://mei-metadata.org/classification")) {
+                let classification_iri = item["https://mei-metadata.org/classification"]["@id"];
+                classification = classifications[classification_iri];
+            }
+            if (item.hasOwnProperty("https://mei-metadata.org/repository")) {
+                let repository_iri = item["https://mei-metadata.org/repository"]["@id"];
+                repository = repositories[repository_iri];
+            }
+            if (item.hasOwnProperty("http://xmlns.com/foaf/0.1/name")) {
+                let person_name_iri = item["http://xmlns.com/foaf/0.1/name"]["@id"];
+                let person_name = person_names[person_name_iri];
+                let person_id = item["@id"];
+                if (!persons.hasOwnProperty(person_id)) {
+                    persons[person_id] = {};
+                }
+                persons[person_id].name = person_name;
+            }
+            if (item.hasOwnProperty("http://www.w3.org/ns/dcat#hadRole")) {
+                let role_iri = item["http://www.w3.org/ns/dcat#hadRole"]["@id"];
+                let role = roles[role_iri];
+                let person_id = item["@id"];
+                if (!persons.hasOwnProperty(person_id)) {
+                    persons[person_id] = {};
+                }
+                persons[person_id].role = role;
+            }
+        });
+
+        let xml = source_template({ title, classification, repository, persons })
+
+        output.querySelector("pre").innerText = xml;
+    }
+});
