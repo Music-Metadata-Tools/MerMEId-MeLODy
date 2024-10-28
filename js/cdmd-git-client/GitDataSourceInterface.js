ba69e0fd90eb12acdcf0818556d95887d4a2938b
claudius <claudius.teodorescu@gmail.com>
2024-10-28 20:49:55 +0200

    Added a interface

---
+++ js/cdmd-git-client/GitDataSourceInterface.js
@@ -0,0 +1,10 @@
+class GitDataSourceInterface {
+    // Loads the repository
+    load_repository() {}
+
+    list_files(parent_dir) {}
+
+    save_file() {}
+
+    commit_file() {}
+}
---
+++ js/cdmd-git-client/LocalGitLabRepo.js
@@ -0,0 +1,19 @@
+/**
+ * @implements  GitDataSourceInterface
+ */
+class LocalGitLabRepo {
+    // loads the repository when 
+    load_repository() {
+
+    }
+
+    list_files(parent_dir) {}
+
+    save_file() {
+
+    }
+
+    commit_file() {
+
+    }
+}
