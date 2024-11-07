6a20211a92fee73af18622224fe8ba2efbbbc688
Schmitz, Annabella <annabella.schmitz@adwmainz.de>
2024-11-07 09:22:03 +0100

    Added first mei serialization for places

--- js/index.js
+++ js/index.js
@@ -36,33 +36,49 @@ const roles = {
 // templates
 const source_template = (data) =>
     `
-         <manifestation>
+        <manifestation>
             <identifier label=""/>
             <titleStmt>
-               <title>${data.title}</title>
-               ${generate_person_records(data.persons)}
+                <title>${data.title}</title>
+                ${generate_person_records(data.persons)}
             </titleStmt>
             <classification>
-               <termList>
-                  <term>${data.classification}</term>
-               </termList>
+                <termList>
+                    <term>${data.classification}</term>
+                </termList>
             </classification>
             <itemList>
-               <item>
-                  <physLoc>
-                     <repository>
-                        <identifier auth.uri="http://www.rism.info" auth="RISM">${data.repository}</identifier>
-                     </repository>
-                  </physLoc>
-               </item>
+                <item>
+                    <physLoc>
+                        <repository>
+                            <identifier auth.uri="http://www.rism.info" auth="RISM">${data.repository}</identifier>
+                        </repository>
+                    </physLoc>
+                </item>
             </itemList>
-         </manifestation>
+        </manifestation>
+`;
+
+const place_template = (data) =>
+    `
+        <geogName type="PlaceOrGeographicName" auth="liszt-portal" auth.uri="https://liszt-portal.de/places/" codedval="1008">
+            <settlement type="city">${data.place_name}</settlement>
+            <name type="variant">${data.alternateName}</name>
+            <country xml:lang="de">${data.country}</country>
+            <creation><date isodate="${data.date}">${data.date}</date></creation>
+            <identifier auth="GND" auth.uri="http://d-nb.info/gnd/" codedval=""/>
+            <desc xml:lang="de">${data.desc}</desc>
+        </geogName>
 `;
 
 const person_template = (data) => `<${data.role}>${data.name}</${data.role}>`;
 
+const identifier_template = (data) => `<identifier auth="${data.auth}" auth.uri="${data.uri}" codedval="${data.id}"/>`;
+
 // util functions
 const generate_person_records = (persons) => Object.values(persons).map(item => person_template(item)).join("");
+
+const generate_identifiers = (identifiers) => Object.values(identifiers).map(item => identifier_template(item)).join("");
 /*
 // Initialize isomorphic-git with a file system
 window.fs = new LightningFS("mermeid");
@@ -72,20 +88,20 @@ window.dir = "/mermeid_sample_data";
 
 // check if dir exists
 try {
-    await pfs.stat(dir);
+await pfs.stat(dir);
 } catch (error) {
-    console.log("clone the repo");
-    await pfs.mkdir(dir);
-    await git.clone({
-        fs,
-        http,
-        dir,
-        corsProxy: 'https://cors.isomorphic-git.org',
-        url: 'https://gitlab.rlp.net/adwmainz/nfdi4culture/cdmd/mermeid-sample-data.git',
-        ref: 'main',
-        singleBranch: true,
-        depth: 1
-    });
+console.log("clone the repo");
+await pfs.mkdir(dir);
+await git.clone({
+fs,
+http,
+dir,
+corsProxy: 'https://cors.isomorphic-git.org',
+url: 'https://gitlab.rlp.net/adwmainz/nfdi4culture/cdmd/mermeid-sample-data.git',
+ref: 'main',
+singleBranch: true,
+depth: 1
+});
 }
 let files = await pfs.readdir(dir + "/data");
 
@@ -93,9 +109,9 @@ let files = await pfs.readdir(dir + "/data");
 const lazyItem = document.querySelector("sl-tree-item");
 
 for (const file of files) {
-    const treeItem = document.createElement("sl-tree-item");
-    treeItem.innerText = file;
-    lazyItem.append(treeItem);
+const treeItem = document.createElement("sl-tree-item");
+treeItem.innerText = file;
+lazyItem.append(treeItem);
 }*/
 
 let form = document.getElementById("shacl-form");
@@ -103,11 +119,11 @@ let xml_output = document.getElementById("shacl-xml-output");
 let rdf_output = document.getElementById("shacl-rdf-output");
 
 /*form.addEventListener("change", (event) => {
-    output.classList.toggle('valid', event.detail?.valid);
-    output.classList.toggle('invalid', !event.detail?.valid);
-    output.querySelector("pre").innerText = form.serialize();
+output.classList.toggle('valid', event.detail?.valid);
+output.classList.toggle('invalid', !event.detail?.valid);
+output.querySelector("pre").innerText = form.serialize();
 
-    console.log(form.serialize());
+console.log(form.serialize());
 });*/
 
 document.addEventListener("click", (event) => {
@@ -121,6 +137,15 @@ document.addEventListener("click", (event) => {
         let classification = "";
         let repository = "";
         let persons = {};
+
+        let place_name = "";
+        let alternate_name = "";
+        let description = "";
+        let country = "";
+        let date = "";
+        let identifiers = {};
+        let type = "";
+
         result_json.forEach((item) => {
             if (item.hasOwnProperty("https://mei-metadata.org/Title")) {
                 title = item["https://mei-metadata.org/Title"]["@value"];
@@ -139,8 +164,8 @@ document.addEventListener("click", (event) => {
                 let person_id = item["@id"];
                 if (!persons.hasOwnProperty(person_id)) {
                     persons[person_id] = {};
-                }
-                persons[person_id].name = person_name;
+            }
+            persons[person_id].name = person_name;
             }
             if (item.hasOwnProperty("http://www.w3.org/ns/dcat#hadRole")) {
                 let role_iri = item["http://www.w3.org/ns/dcat#hadRole"]["@id"];
@@ -151,11 +176,32 @@ document.addEventListener("click", (event) => {
                 }
                 persons[person_id].role = role;
             }
+            if (item.hasOwnProperty("http://schema.org/name")) {
+                place_name = item["http://schema.org/name"]["@value"];
+            }
+            if (item.hasOwnProperty("https://d-nb.info/standards/elementset/gnd#PlaceOrGeographicName")) {
+                let type_iri = item["https://d-nb.info/standards/elementset/gnd#PlaceOrGeographicName"]["@value"];
+                type = types[type_iri];
+            }
+            if (item.hasOwnProperty("http://schema.org/alternateName")) {
+                alternate_name = item["http://schema.org/alternateName"]["@value"];
+            }
+            if (item.hasOwnProperty("http://schema.org/description")) {
+                description = item["http://schema.org/description"]["@value"];
+            }
+            if (item.hasOwnProperty("http://www.cidoc-crm.org/cidoc-crm/P89_falls_within")) {
+                country = item["http://www.cidoc-crm.org/cidoc-crm/P89_falls_within"]["@value"];
+            }
+            if (item.hasOwnProperty("https://d-nb.info/standards/elementset/gnd#dateOfEstablishment")) {
+                date = item["https://d-nb.info/standards/elementset/gnd#dateOfEstablishment"]["@value"];
+            }
         });
 
         let xml = source_template({ title, classification, repository, persons })
+        let place_xml = place_template({ place_name, alternate_name, country, date, desc })
 
-        xml_output.querySelector("pre").innerText = xml;
+        //xml_output.querySelector("pre").innerText = xml;
+        xml_output.querySelector("pre").innerText = place_xml;
         rdf_output.querySelector("pre").innerText = rdf_result;
     }
 });
